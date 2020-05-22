5e544d3dcd36eca2231c8a4edff441a55b8a3e3f
Alejandro Batres <ComplexRalex@users.noreply.github.com>
2020-05-21 20:58:11 -0500

    Presentación mejorada e inclusión de introducción

--- main.cpp
+++ main.cpp
@@ -13,15 +13,16 @@
 /*! \mainpage Presentación del trabajo
  * 
  * <center>
- * <table style="max-width: 650px;">
+ * <div style="max-width: 750px; text-align: justify;">
+ * <table style="align: center; width: 100%">
  * <tr><td style="padding: 15px;"><center><img src="./img/fcc_logo.png"></center><td style="padding: 15px;"><center><img src="./img/BUAP_logo.png"></center>
- * <tr><td align="center" style="padding: 5px; font-size: 15px;"><i><b>Facultad de Ciencias de la Computación</b></i><td align="center" style="padding: 5px; font-size: 15px;"><i><b>Benemértia Universidad Autónoma de Puebla</b></i>
+ * <tr><td align="center" style="padding: 5px; font-size: 15px;"><i><b>Facultad de Ciencias de la Computación</b></i><td align="center" style="padding: 5px; font-size: 15px;"><i><b>Benemérita Universidad Autónoma de Puebla</b></i>
  * <tr><td colspan="2" align="center" style="padding: 5px;"><b>Profesora</b> María del Carmen Santiago Díaz
  * <tr><td colspan="2" align="center" style="padding: 5px;">Estructuras de Datos
  * <tr><td colspan="2" align="center" style="padding: 5px; font-size: 20px;"><i><b>Manual técnico de TDAs y Árboles AVL</b></i>
  * </table>
  * <br>
- * <table class="team" style="max-width: 650px; border: 2px black solid;">
+ * <table class="team" style="border: 2px black solid; align: center; width: 100%">
  * <tr><th colspan="2" style="background: black; color: white;">Integrantes del equipo 3
  * <tr><td style="border: 1px black solid; padding: 2px;">Batres Pedroza Alejandro<td style="border: 1px black solid; padding: 2px;">201836943
  * <tr><td style="border: 1px black solid; padding: 2px;">García González Jorge<td style="border: 1px black solid; padding: 2px;">201847512
@@ -29,8 +30,7 @@
  * <tr><td style="border: 1px black solid; padding: 2px;">Segura Cuanalo Ricardo Alejandro<td style="border: 1px black solid; padding: 2px;">201848777
  * </table>
  * <br>
- * <h2>Introducción
- * <div style="max-width: 650px;">
+ * <h2 style="align: center;">Introducción</h2>
  * Este manual técnico fue realizado con el propósito de brindar una mejor comprensión para cualquiera que busque utilizar nuestros TDAs, ya que esto
  * podría ayudar al usuario a emplear nuestros algoritmos para diferentes tareas. Esto se pretende lograr, utilizando descripciones concisas para los
  * métodos empleados, los parámetros de estos y las variables que contiene cada clase utilizada en el código.
